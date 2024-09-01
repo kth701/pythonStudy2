74f4b10f25a1caa4e66a21ef8ea35975e6db65b6
kth701 <kth701_nate.com>
2024-09-01 10:10:23 +0900

    - dict test02

--- test02/ex04.py
+++ test02/ex04.py
@@ -15,13 +15,19 @@ char = {
     "skill": ["베기","세게 베기", "아주 세게 베기"]
 }
 
-for key in char:
-    # print(char[key], type(char[key]))
+for key in char: # dict자료형에 있는 key를 순차적으로 호출
+    # print(key, char[key], type(char[key]))
 
     if type(char[key]) is dict:
-        for small_key in char[key]:
+        for small_key in char[key]: # char[key] dict의 key를 순차적으로 호출
+            # key2 = char[key]
+            # print(key2[small_key])
             print(char[key][small_key])
-    elif type(char[key]) is list:
-        print(key)
+
+    elif type(char[key]) is list: 
+
+        for item in char[key]:# list자료형 데이터를 순차적으로 호출
+            print(item)
+            
     else:
         print(char[key])
---
+++ test02/test.py
@@ -0,0 +1,8 @@
+num = 300
+print(num)
+list_a = [10,20,[1,2,[100,200]]]
+print(list_a)
+list_a[1] = 300
+print(list_a[0], list_a[1], list_a[2])
+list_a[2][1] = 1000
+print(list_a[2][1])
